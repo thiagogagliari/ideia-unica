d3f710779975218d2d9e640264ba2e1dafb4b37f
thiagogagliari <81534095+thiagogagliari@users.noreply.github.com>
2021-04-23 23:56:31 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const participantes = ['Téo','Jonathan','Gleiton','Carlos','Miguel','Isabella','Luan','Ivanir','Boninho','Joãozinho','Marcos','Guilherme'];
-
-const sorteioAleatorio = participantes[Math.floor(Math.random() * participantes.length)];
-
-document.write('<h1> O Ganhador é: <br>', sorteioAleatorio + '<h1>');
-
